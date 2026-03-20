8bd2479f8f13076fd06190db240403d08930259a
keshav <keshav.aggarwal.2509@gmail.com>
2026-03-20 18:44:30 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/concurrentdb/concurrent_db/ConcurrentDbApplication.java
@@ -0,0 +1,13 @@
+package com.concurrentdb.concurrent_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrentDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/concurrentdb/concurrent_db/ConcurrentDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.concurrentdb.concurrent_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
